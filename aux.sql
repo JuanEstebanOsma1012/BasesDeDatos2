3ed811c0e8faf3dbc38d3b509ad8f675262860a3
Juan Esteban Castaño Osma <osmajuan760@gmail.com>
2024-05-06 23:13:42 -0500

    agregado el script de poblacion (con fallas) y la actividad de los triggers
    se ha agregado el script de poblacion de la base de datos con fallas en la creacion del examen (por culpa del trigger) y error en la creacion de las tablas intermedias por los modulos (error de constraints) además, el segundo trigger tambien da error de constraints, hay que revisarlo

--- aux.sql
+++ aux.sql
@@ -8,3 +8,59 @@ GRANT DBA TO "C##PROYECTO_FINAL_PRUEBAS";
 INSERT INTO docente (nombre, apellido, identificacion) VALUES ('qwertyuiopasdfghjklñzxcvbnmqwertyuiopasdfghjklñzxcvbnmqwertyu', 'qwertyuiopasdfghjklñzxcvbnmqwertyuiopasdfghjklñzxcvbnmqwertyu', '1234567890123456789012345678901');
 DELETE FROM docente WHERE identificacion = '1234567890123456789012345678901';
 SELECT * FROM docente WHERE identificacion = '1234567890123456789012345678901';
+
+-- ----------------------------- desde aquí empieza el taller de Triggers que puso Triviño para una "entrega parcial" del parcial ------------------------------
+
+-- Cree un disparador que garantice que un examen no puede ser modificado si existe algún alumno que ya lo presentó
+
+CREATE OR REPLACE TRIGGER evitar_modificacion_examen_presentado
+BEFORE UPDATE ON examen
+FOR EACH ROW
+DECLARE
+  v_presentaciones_existentes INTEGER;
+BEGIN
+  SELECT COUNT(*) INTO v_presentaciones_existentes
+  FROM presentacion_examen
+  WHERE id_examen = :OLD.id_examen;
+
+  IF v_presentaciones_existentes > 0 THEN
+    RAISE_APPLICATION_ERROR(-20001, 'No se puede modificar el examen porque ya ha sido presentado por alumnos.');
+  END IF;
+END;
+
+-- cree un disparador que cuando cree el examen inserte de forma aleatoria las n preguntas que requiera
+
+CREATE OR REPLACE TRIGGER insertar_preguntas_aleatorias
+AFTER INSERT ON examen
+FOR EACH ROW
+DECLARE
+  v_preguntas_disponibles INTEGER;
+  v_pregunta_seleccionada INTEGER;
+BEGIN
+  SELECT COUNT(*) INTO v_preguntas_disponibles
+  FROM pregunta
+  WHERE id_tema = :NEW.id_tema AND es_publica = 'S';
+
+  FOR i IN 1..:NEW.numero_preguntas LOOP
+    v_pregunta_seleccionada := DBMS_RANDOM.VALUE(1, v_preguntas_disponibles);
+
+    INSERT INTO pregunta_examen (id_examen, id_pregunta, tiene_tiempo_maximo)
+    SELECT :NEW.id_examen, id_pregunta, 'S'
+    FROM (
+      SELECT id_pregunta
+      FROM pregunta
+      WHERE id_tema = :NEW.id_tema AND es_publica = 'S'
+      ORDER BY DBMS_RANDOM.RANDOM
+    )
+    WHERE ROWNUM = v_pregunta_seleccionada;
+  END LOOP;
+END;
+
+-- cree un disparador que establezca la fecha y hora en la cual el alumno empezó a realizar el examen.
+
+CREATE OR REPLACE TRIGGER registrar_fecha_hora_inicio_examen
+BEFORE INSERT ON presentacion_examen
+FOR EACH ROW
+BEGIN
+  :NEW.fecha_presentacion := SYSDATE;
+END;
