b9280291bccafb9924710c141ef3dd0e56039048
Juan Esteban Castaño Osma <osmajuan760@gmail.com>
2024-05-11 03:28:35 -0500

    agregado un nuevo atributo a la tabla 'pregunta'
    se ha agregado el atributo 'estado' a la tabla 'pregunta' para tener capacidad de hacer una eliminacion logica

--- DDL_creacion.sql
+++ DDL_creacion.sql
@@ -1,14 +1,3 @@
--- Generado por Oracle SQL Developer Data Modeler 23.1.0.087.0806
---   en:        2024-05-11 02:15:06 COT
---   sitio:      Oracle Database 11g
---   tipo:      Oracle Database 11g
-
-
-
--- predefined type, no DDL - MDSYS.SDO_GEOMETRY
-
--- predefined type, no DDL - XMLTYPE
-
 CREATE TABLE alumno (
     id_alumno INTEGER NOT NULL,
     nombre    VARCHAR2(63) NOT NULL,
@@ -147,7 +136,8 @@ CREATE TABLE pregunta (
     tipo_pregunta         VARCHAR2(32) NOT NULL,
     id_pregunta_compuesta INTEGER,
     id_tema               INTEGER NOT NULL,
-    id_docente            INTEGER NOT NULL
+    id_docente            INTEGER NOT NULL,
+    estado                VARCHAR2(15) NOT NULL
 );
 
 CREATE INDEX pregunta_tipo_pregunta_idx ON
@@ -465,48 +455,3 @@ BEGIN
     :new.id_unidad := unidad_id_unidad_seq.nextval;
 END;
 /
-
-
-
--- Informe de Resumen de Oracle SQL Developer Data Modeler: 
--- 
--- CREATE TABLE                            16
--- CREATE INDEX                            15
--- ALTER TABLE                             38
--- CREATE VIEW                              0
--- ALTER VIEW                               0
--- CREATE PACKAGE                           0
--- CREATE PACKAGE BODY                      0
--- CREATE PROCEDURE                         0
--- CREATE FUNCTION                          0
--- CREATE TRIGGER                          12
--- ALTER TRIGGER                            0
--- CREATE COLLECTION TYPE                   0
--- CREATE STRUCTURED TYPE                   0
--- CREATE STRUCTURED TYPE BODY              0
--- CREATE CLUSTER                           0
--- CREATE CONTEXT                           0
--- CREATE DATABASE                          0
--- CREATE DIMENSION                         0
--- CREATE DIRECTORY                         0
--- CREATE DISK GROUP                        0
--- CREATE ROLE                              0
--- CREATE ROLLBACK SEGMENT                  0
--- CREATE SEQUENCE                         12
--- CREATE MATERIALIZED VIEW                 0
--- CREATE MATERIALIZED VIEW LOG             0
--- CREATE SYNONYM                           0
--- CREATE TABLESPACE                        0
--- CREATE USER                              0
--- 
--- DROP TABLESPACE                          0
--- DROP DATABASE                            0
--- 
--- REDACTION POLICY                         0
--- 
--- ORDS DROP SCHEMA                         0
--- ORDS ENABLE SCHEMA                       0
--- ORDS ENABLE OBJECT                       0
--- 
--- ERRORS                                   0
--- WARNINGS                                 0
