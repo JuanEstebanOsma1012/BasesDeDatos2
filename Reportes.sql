2d06b39e8bc9d1527413ec6cfb1b735fe46364af
Juan Esteban Castaño Osma <osmajuan760@gmail.com>
2024-05-12 18:37:10 -0500

    se ha diligenciado las vistas necesarias para el sistema
    se han puesto las vistas necesarias para el proyecto, por cada rol y para los diferentes reportes que las requieren

--- Reportes.sql
+++ Reportes.sql
@@ -1,16 +1,4 @@
 --  Reporte de rendimiento promedio por tema y docente, por tema y general
-
--- Vista para obtener el rendimiento promedio por alumno y tema
-CREATE OR REPLACE VIEW RendimientoAlumnoTema AS
-SELECT n.id_alumno, t.id_tema, AVG(n.valor) as rendimiento
-FROM nota n
-JOIN alumno_grupo ag ON n.id_alumno = ag.id_alumno
-JOIN grupo g ON ag.id_grupo = g.id_grupo
-JOIN examen e ON g.id_grupo = e.id_grupo
-JOIN tema t ON e.id_tema = t.id_tema
-GROUP BY n.id_alumno, t.id_tema;
-
--- Reporte de rendimiento promedio 
 SELECT 
     t.titulo AS Tema, 
     d.nombre || ' ' || d.apellido AS Docente, 
@@ -28,7 +16,6 @@ JOIN docente d ON e.id_docente = d.id_docente
 GROUP BY ROLLUP (t.titulo, d.nombre || ' ' || d.apellido);
 
 -- Reporte de calificaciones por trimestre del año inmediatamente anterior
-
 SELECT id_alumno,
        TRIM(TO_CHAR(Trimestre1, '999.99')) AS Trimestre1,
        TRIM(TO_CHAR(Trimestre2, '999.99')) AS Trimestre2,
@@ -57,7 +44,6 @@ PIVOT (
 ORDER BY id_alumno;
 
 -- Cantidad de preguntas por tema y profesor, por tema, por profesor y en general
-
 SELECT 
     t.titulo AS Tema, 
     d.nombre || ' ' || d.apellido AS Docente, 
@@ -68,13 +54,6 @@ JOIN docente d ON p.id_docente = d.id_docente
 GROUP BY GROUPING SETS (t.titulo, d.nombre || ' ' || d.apellido, ());
 
 -- Cantidad de horas promedio al día de trabajo de cada docente
-
--- Vista para calcular la duración de cada bloque horario
-CREATE OR REPLACE VIEW DuracionBloqueHorario AS
-SELECT id_bloque_horario, (EXTRACT(HOUR FROM hora_fin) - EXTRACT(HOUR FROM hora_inicio)) AS Duracion 
-FROM bloque_horiario;
-
--- Reporte de horas promedio al día de trabajo por docente
 SELECT 
     d.nombre || ' ' || d.apellido AS Docente, 
     ROUND(AVG(dbh.Duracion),2) AS HorasPromedioDia
